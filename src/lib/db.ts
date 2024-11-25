6f2974e635115a96940f442b39000204bbbf4fb4
Ar9av <f2016731@pilani.bits-pilani.ac.in>
2024-11-25 11:20:47 +0530

    fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -11,14 +11,14 @@
 import { Pool, neonConfig } from '@neondatabase/serverless'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { PrismaClient } from '@prisma/client'
-
-// import dotenv from 'dotenv'
 import ws from 'ws'
 
-// dotenv.config()
+// Setup
 neonConfig.webSocketConstructor = ws
-// const connectionString = `${process.env.DATABASE_URL}`
-const pool = new Pool()
+const connectionString = `${process.env.DATABASE_URL}`
+
+// Init prisma client
+const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
 const prisma = new PrismaClient({ adapter })
 
